10cd58589d73dc27bd2580654aa247606c59c740
AnnieChris <annieesther3@gmail.com>
2022-04-12 02:36:21 -0500

    new updates

--- java-basics/src/com/training/mars/MyClass.java
+++ java-basics/src/com/training/mars/MyClass.java
@@ -3,7 +3,8 @@ package com.training.mars;
 public class MyClass {
 	
 	public static void main(String args[]) {
-		System.out.println("This is my first Java file");
+		System.out.println("This is my First Java file");
+		System.out.println("New Change");
 	}
 
 }
