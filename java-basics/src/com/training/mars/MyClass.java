58a888e03979a7a8c79ed6b870b01db46cf43fcd
AnnieChris <annieesther3@gmail.com>
2022-04-12 01:34:17 -0500

    Initial commit

---
+++ java-basics/src/com/training/mars/MyClass.java
@@ -0,0 +1,10 @@
+package com.training.mars;
+
+public class MyClass {
+	
+	public static void main(String args[]) {
+		System.out.println("This is my first Java file");
+	}
+
+}
+//Access Specifiers : public => anyone can use this
